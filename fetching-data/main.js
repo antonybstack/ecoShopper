2faf04eea0c2bfafbac679c0ee2c2abe82f37f92
Rubens Jean-Paul <rubens.jeanpaul@gmail.com>
2019-11-02 18:47:29 -0400

    finally displaying information on the front end

--- fetching-data/main.js
+++ fetching-data/main.js
@@ -1,18 +1,26 @@
 let submitButton = document.getElementById("submit-button");
+let suggestionArea = document.getElementById("results");
 submitButton.addEventListener("click", function (event) {
     event.preventDefault();
     let productQuery = document.getElementById("product").value.trim();
     let url = `https://serpapi.com/search?q=${productQuery}&tbm=shop&api_key=5680df1256b257af71c6601da987d01181bc5a80caa5eefd4fa51b5636e6e4c8`;
-
-    fetch(url)
-        .then(res =>
-            res.json()
-        )
-        .then(data => {
-            console.log(data.shopping_results);
-        })
-        .catch(err => console.log(err));
-
+    async function getData(url) {
+        const response = await fetch(url).catch(err => err);
+        const myJson = await response.json();
+        console.log(myJson.shopping_results);
+        suggestionArea.innerHTML = "";
+        for (suggestions in myJson.shopping_results) {
+            let results = myJson.shopping_results[suggestions];
+            let suggestionHTML = `<div class="suggestion-item">
+            <img src="${results.thumbnail ? results.thumbnail : "eco_image.jpg" }" alt="${results.title}" width="37px" height="30px">
+            <span>${results.title}</span>
+            <span>${results.price}</span>
+            <a title="${results.title}" href="${results.link}" target="_blank">Buy this</a>
+          </div>`
+            suggestionArea.insertAdjacentHTML('beforeend', suggestionHTML);
+        }
+    }
+    getData(url);
     console.log(productQuery);
     console.log(url);
 });
